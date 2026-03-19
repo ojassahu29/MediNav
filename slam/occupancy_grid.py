dab38bef7cee82fc6f260a76fa4f6841ccf04ebb
Aadityahv <aadityahv1711@gmail.com>
2026-03-19 12:51:08 +0530

    Save occupancy grid as .npy file for use by risk module

--- slam/occupancy_grid.py
+++ slam/occupancy_grid.py
@@ -173,6 +173,7 @@ def main():
         os.makedirs('outputs')
         
     plt.savefig('outputs/occupancy_grid.png', dpi=150)
+    np.save('outputs/occupancy_grid.npy', p_map)
     print("Saved plot to outputs/occupancy_grid.png")
 
 if __name__ == '__main__':
